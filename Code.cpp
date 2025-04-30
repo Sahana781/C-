d4c81cbdad334b0446ff8934ff7c31db76c7d75c
Sahana K P <sahanap409@gmail.com>
2025-04-30 05:41:15 -0700

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,83 +1,83 @@
-#include<iostream>
-#include<string>
-#include<bitset>
+#include <iostream>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
-bool isValidHex (const String & hexstr) {
-	for(char c:hexstr){
-		if(!isxdigit(c)){
-			return false;
-		}
-	}
-	return false;
+bool isValidHex(const string& hexstr) {
+    for (char c : hexstr) {
+        if (!isxdigit(c)) {
+            return false;
+        }
+    }
+    return true;
 }
 
-String hexDigitToBinary(char hexDigit){
-	switch(toupper(hexDigit))
-	{
-		Case'0':return"0000";
-	    Case'1':return"0001";
-	    Case'2':return"0010";
-	   	Case'3':return"0011";
-	   	Case'4':return"0100";
-	    Case'5':return"0101";
-	    Case'6':return"0110";
-	   	Case'7':return"0111";
-	  	Case'8':return"1000";
-	  	Case'8':return"1001";
-	  	
-	  	
-	  	Case'a':return"1010";
-	    Case'b':return"1011";
-	    Case'c':return"1100";
-	   	Case'd':return"1101";
-	   	Case'e':return"1110";
-	    Case'f':return"1111";
-	   
-	}
+string hexDigitToBinary(char hexDigit) {
+    switch (toupper(hexDigit)) {
+        case '0': return "0000";
+        case '1': return "0001";
+        case '2': return "0010";
+        case '3': return "0011";
+        case '4': return "0100";
+        case '5': return "0101";
+        case '6': return "0110";
+        case '7': return "0111";
+        case '8': return "1000";
+        case '9': return "1001";
+        case 'A': return "1010";
+        case 'B': return "1011";
+        case 'C': return "1100";
+        case 'D': return "1101";
+        case 'E': return "1110";
+        case 'F': return "1111";
+        default: return "????";
+    }
 }
 
-int main(){
-	String labelsets[6][16]={
-		{
-			{"As","Ab","CD","EF","GH"."IJ","KL","MN","As","Ab","CD","EF","GH"."IJ","KL","MN"}
-			{"As","Ab","CD","EF","GH"."IJ","KL","MN","As","Ab","CD","EF","GH"."IJ","KL","MN"}
-			{"As","Ab","CD","EF","GH"."IJ","KL","MN","As","Ab","CD","EF","GH"."IJ","KL","MN"}
-			{"As","Ab","CD","EF","GH"."IJ","KL","MN","As","Ab","CD","EF","GH"."IJ","KL","MN"}
-			{"As","Ab","CD","EF","GH"."IJ","KL","MN","As","Ab","CD","EF","GH"."IJ","KL","MN"}
-			{"As","Ab","CD","EF","GH"."IJ","KL","MN","As","Ab","CD","EF","GH"."IJ","KL","MN"}
-			
-		};
-		for(int iteration=1;iterarion<=6;++iteration){
-			cout<<"\n__bit"<<iteration <<"-----" <<endl;
-			String hexInput;
-			if(hexInput=="Quit"){
-				cout<<"Good Bye"<<endl;
-				return 0;
-			}
-			if(!isValidHex(hexInput)){
-				break;
-			}
-			else{
-				cout<<"  " <<endl;
-			}
-			String*labels=labelsets[iteration-1];
-			int labelIndex=0;
-			cout<<"Binary Equivalent of hexadecimal:"<<hexInput<<endl;
-            for(char hexDigit:hexInput){
-                String binary=hexDigitToBinary(hexDigit);
-			for(char bit:binary){
-                cout<<bit;
-                if(bit=='1'){
-				cout<<"---->"  <<labels[labelIndex];
-				
-			}
-			cout<<endl;
-			labelIndex=(labelIndex+1)%15;
-		}
-		
-	}
+int main() {
+    string labelsets[6][16] = {
+        {"As","Ab","CD","EF","GH","IJ","KL","MN","As","Ab","CD","EF","GH","IJ","KL","MN"},
+        {"As","Ab","CD","EF","GH","IJ","KL","MN","As","Ab","CD","EF","GH","IJ","KL","MN"},
+        {"As","Ab","CD","EF","GH","IJ","KL","MN","As","Ab","CD","EF","GH","IJ","KL","MN"},
+        {"As","Ab","CD","EF","GH","IJ","KL","MN","As","Ab","CD","EF","GH","IJ","KL","MN"},
+        {"As","Ab","CD","EF","GH","IJ","KL","MN","As","Ab","CD","EF","GH","IJ","KL","MN"},
+        {"As","Ab","CD","EF","GH","IJ","KL","MN","As","Ab","CD","EF","GH","IJ","KL","MN"}
+    };
+
+    for (int iteration = 1; iteration <= 6; ++iteration) {
+        cout << "\n__bit" << iteration << "-----" << endl;
+        string hexInput;
+        cout << "Enter a hexadecimal string (or 'Quit' to exit): ";
+        cin >> hexInput;
+
+        if (hexInput == "Quit") {
+            cout << "Good Bye" << endl;
+            return 0;
+        }
+
+        if (!isValidHex(hexInput)) {
+            cout << "Invalid hexadecimal input." << endl;
+            continue;
+        }
+
+        string* labels = labelsets[iteration - 1];
+        int labelIndex = 0;
+        cout << "Binary Equivalent of hexadecimal: " << hexInput << endl;
+
+        for (char hexDigit : hexInput) {
+            string binary = hexDigitToBinary(hexDigit);
+            for (char bit : binary) {
+                cout << bit;
+                if (bit == '1') {
+                    cout << " ----> " << labels[labelIndex];
+                }
+                cout << endl;
+                labelIndex = (labelIndex + 1) % 16;
+            }
+        }
+    }
+
+    return 0;
 }
-return 0;
-}
+
